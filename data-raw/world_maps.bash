4e560cb1cf8b3c6db37a00a9529fe5b6d5511c8a
Edwin de Jonge <edwindjonge@gmail.com>
2017-04-20 22:43:29 +0200

    adding files

---
+++ data-raw/world_maps.bash
@@ -0,0 +1,31 @@
+curl -O http://ec.europa.eu/eurostat/cache/GISCO/geodatafiles/CNTR_2014_03M_SH.zip
+
+unzip -o CNTR_2014_03M_SH.zip
+
+rm -r ../data
+mkdir ../data
+
+MDFILE="../index.md"
+
+echo -e "# World maps\n" > $MDFILE
+
+for proj in "wintri" "eck4" "kav7" "wgs84" "robinson"
+do
+  echo -e "## Projectie: ${proj}\n" >> $MDFILE
+  mapshaper -i CNTR_2014_03M_SH/Data/CNTR_RG_03M_2014.shp \
+   -filter 'CNTR_ID != "AQ"' \
+   -proj $proj densify \
+   -simplify 3% \
+   -o ../data/world_${proj}.shp
+
+  mkdir -p ../${proj}
+  mapshaper -i ../data/world_${proj}.shp \
+   -o ../${proj}/world.geojson id-field=CNTR_ID
+   echo "[world.geojson](${proj}/world.geojson)" >> $MDFILE
+
+  mapshaper -i ../data/world_${proj}.shp \
+   -o ../${proj}/world.topojson id-field=CNTR_ID
+   echo "[world.geojson](${proj}/world.geojson)" >> $MDFILE
+done
+
+rm -r CNTR_2014_03M_SH
